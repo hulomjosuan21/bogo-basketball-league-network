0008330457f47d2df83feb7ad9a9162568702be6
Josuan Leonardo Hulom <110836440+hulomjosuan21@users.noreply.github.com>
2024-12-29 00:53:52 +0800

    sms working

---
+++ src/actions/sendSmsActions.ts
@@ -0,0 +1,65 @@
+'use server'
+import AppToolkit from "@/lib/app-toolkit";
+
+import { NextResponse } from 'next/server';
+
+const infobipApiKey = process.env.NEXT_PUBLIC_INFODIP_API_KEY!;
+
+if(!infobipApiKey){
+    throw new Error('Infodip apy key is mission')
+}
+
+export async function SendMessageRequest(request:NextResponse) {
+    const { to, text } = await request.json();
+
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `App ${infobipApiKey}`);
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Accept", "application/json");
+
+    const raw = JSON.stringify({
+        "messages": [
+            {
+                "destinations": [{"to": to}],
+                "from": "Bogo Basketball League Network",
+                "text": text
+            }
+        ]
+    });
+
+    const requestOptions: RequestInit = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    try {
+        const response = await fetch("https://4e29r8.api.infobip.com/sms/2/text/advanced", requestOptions);
+        const result = await response.json();
+        return NextResponse.json(result);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: "Failed to send SMS" }, { status: 500 });
+    }
+}
+
+
+export async function sendSmsAction(to: string, text: string) {
+    const request = {
+        json: async () => ({ to , text })
+    } as never;
+
+    try {
+        const response = await SendMessageRequest(request);
+        const result = await response.json();
+
+        if (response.status !== 200) {
+            return { errorMessage: result.message || 'Failed to send SMS' };
+        }
+
+        return { errorMessage: null };
+    } catch (error) {
+        return { errorMessage: AppToolkit.getErrorMessage(error,'Failed to send SMS') };
+    }
+}
--- src/app/(entities)/player/page.tsx
+++ src/app/(entities)/player/page.tsx
@@ -1,7 +1,32 @@
+'use client'
+import {Button} from "@/components/ui/button";
+import {sendSmsAction} from "@/actions/sendSmsActions";
+import Form from "next/form";
+import {Input} from "@/components/ui/input";
+
 export default function Page(){
+
+    const handleSend = async (formData: FormData) => {
+        const text = formData.get('text') as string
+        const { errorMessage } = await sendSmsAction('639682006456',text)
+
+        if (errorMessage) {
+            alert(errorMessage)
+        } else {
+            alert('SMS sent')
+        }
+    }
+
     return (
         <main className={''}>
-            Player Page
+            <Form action={handleSend}>
+                <div className={'flex items-center gap-4'}>
+                    <Input placeholder={'Text'} name={'text'} />
+                    <Button>
+                        Send
+                    </Button>
+                </div>
+            </Form>
         </main>
     )
 }
